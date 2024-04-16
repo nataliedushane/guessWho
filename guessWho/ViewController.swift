24e54f85c90ded877aa8ae341fae21bc6856b530
Mykaela Wallen <mykaelawallen@gmail.com>
2024-04-16 09:35:00 -0500

    testing pulling - should get names and keep anems

--- guessWho/ViewController.swift
+++ guessWho/ViewController.swift
@@ -10,9 +10,29 @@ import FirebaseCore
 import FirebaseDatabase
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let alertController = UIAlertController(title: "Sign in!", message: "", preferredStyle: UIAlertController.Style.alert)
+        alertController.addTextField { (textField : UITextField!) -> Void in
+                textField.placeholder = "Enter Name"
+            }
+        let saveAction = UIAlertAction(title: "Save", style: UIAlertAction.Style.default, handler: { alert -> Void in
+        let firstTextField = alertController.textFields![0] as UITextField
+            var blah = [firstTextField.text! : AppData.names]
+            print(blah)
+            //self.ref.child("decks").childByAutoId().setValue(blah)
+           // self.newDeck = []
+           // self.table.reloadData()
+            })
+        
+            
+            alertController.addAction(saveAction)
+            
+        self.present(alertController, animated: true, completion: nil)
         // Do any additional setup after loading the view.
     }
 
