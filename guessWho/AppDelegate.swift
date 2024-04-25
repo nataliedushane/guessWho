d67429553f1b3a9a73cb501607a2dd854e536f6a
Mykaela Wallen <mykaelawallen@gmail.com>
2024-04-25 09:09:54 -0500

    maybe works on phone

--- guessWho/AppDelegate.swift
+++ guessWho/AppDelegate.swift
@@ -33,23 +33,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        print("discard scenes")
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
         ref = Database.database().reference()
-
+        
         self.ref.child("names").child(AppData.key).removeValue()
 
-        print("i removed the name!")
+        self.ref.childByAutoId().setValue("removed")
+        print("terminated app")
+        
+        
 
     }
+    
+  
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         print("hello")
         ref = Database.database().reference()
 
         self.ref.child("names").child(AppData.key).removeValue()
-
-        print("i removed the name!")
+        self.ref.childByAutoId().setValue("removed")
+        print("enter backckground")
 
     }
 
