9aba8a9c8aba6b13804cbd4224b7212c9ee4b324
Natalie DuShane <natdd1010@gmail.com>
2024-05-01 09:19:29 -0500

    lieuhtroiu

--- guessWho/ViewController.swift
+++ guessWho/ViewController.swift
@@ -20,46 +20,57 @@ class ViewController: UIViewController {
         ref = Database.database().reference()
         AppData.wins = defaults.integer(forKey: "theWins")
         AppData.losses = defaults.integer(forKey: "theLosses")
-        let alertController = UIAlertController(title: "Sign in!", message: "", preferredStyle: UIAlertController.Style.alert)
-        alertController.addTextField { (textField : UITextField!) -> Void in
+        
+        if(defaults.string(forKey: "theName") != nil){
+            AppData.singleName = defaults.string(forKey: "theName")!
+            print("\(AppData.singleName)")
+        }
+        else{
+            
+            
+            let alertController = UIAlertController(title: "Sign in!", message: "", preferredStyle: UIAlertController.Style.alert)
+            alertController.addTextField { (textField : UITextField!) -> Void in
                 textField.placeholder = "Enter Name"
             }
-        let saveAction = UIAlertAction(title: "Save", style: UIAlertAction.Style.default, handler: { alert -> Void in
-        let firstTextField = alertController.textFields![0] as UITextField
-            var blah = firstTextField.text!
-            AppData.key = self.ref.child("names").childByAutoId().key ?? "0"
-            
-            AppData.singleName = "\(blah)"
-            AppData.names.append(blah)
-            print(blah)
-            print(AppData.singleName)
-            self.ref.child("names").childByAutoId().setValue(AppData.names)
-            AppData.names = [""]
-
-            self.ref.child("names").observe(.childAdded, with: { (snapshot) in
-                // snapshot is a dictionary with a key and a value
+            let saveAction = UIAlertAction(title: "Save", style: UIAlertAction.Style.default, handler: { alert -> Void in
+                let firstTextField = alertController.textFields![0] as UITextField
+                var blah = firstTextField.text!
+                AppData.key = self.ref.child("names").childByAutoId().key ?? "0"
                 
-                // this gets each name from each snapshot
-                let n = snapshot.value as! [String]
-                // adds the name to an array if the name is not already there
-                AppData.names.append(contentsOf:  n)
-                    print(n)
+                AppData.singleName = "\(blah)"
+                self.defaults.set(AppData.singleName, forKey: "theName")
                 
-                })
-            
-            
-           
-
-            
+                AppData.names.append(blah)
+                print(blah)
+                print(AppData.singleName)
+                self.ref.child("names").childByAutoId().setValue(AppData.names)
+                AppData.names = [""]
+                
+                                
+                
+                
+                
+                
             })
-        
-        
-        
+            
+            
+            
             
             alertController.addAction(saveAction)
             
-        self.present(alertController, animated: true, completion: nil)
-        
+            self.present(alertController, animated: true, completion: nil)
+        }
+        self.ref.child("names").observe(.childAdded, with: { (snapshot) in
+            // snapshot is a dictionary with a key and a value
+            
+            // this gets each name from each snapshot
+            let n = snapshot.value as! [String]
+            // adds the name to an array if the name is not already there
+            AppData.names.append(contentsOf:  n)
+            print(n)
+            
+        })
+
         
         ref.child("names").observe(.childRemoved, with: { (snapshot) in
             // snapshot is a dictionary with a key and a value
